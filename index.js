2bc5be1dca97f6d24175aa653e2157d2db2c1f1a
londybaz420 <freedeployment101@gmail.com>
2025-09-22 15:07:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`
-HELLO SIR
+server File Is Ready to Use
 
 Server running on http://localhost:` + PORT)
 });
